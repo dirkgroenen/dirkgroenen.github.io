63619987ad5e316c7417910b2da9fb0a3da90d24
Dirk Groenen <dirk@bitlabs.nl>
2020-04-05 00:51:24 +0200

    Remove jquery

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,80 +1,31 @@
-(function ($) {
-    function verifyAuthorInfoColor() {
-        if ($(document).scrollTop() > $("header.image.singlepost").height() - 30) {
-            $("a.openmenu, .authorinfo:not(.open)").removeClass("white");
+(function () {
+    function verifyAuthorInfoColor(checkElm, modElm) {
+        if (window.scrollY > checkElm.clientHeight - 30) {
+            modElm.classList.remove("white");
         }
         else {
-            $("a.openmenu, .authorinfo").addClass("white");
+            modElm.classList.add("white");
         }
     }
 
-    $(document).ready(function () {
-        $(".postcontent a").each(function () {
-            if ($(this).attr("href").substr(0, 1) != "#")
-                $(this).attr("target", "_blank");
-        });
+    window.addEventListener('DOMContentLoaded', function () {
+        const authorInfoElm = document.querySelector(".authorinfo");
+        const headerImageElm = document.querySelector('header.image.singlepost');
 
-        // Parallax image for post top
-        $.parallax({ speed: .9 });
+        document.querySelectorAll('.postcontent a').forEach(a => {
+            if (!a.getAttribute('href').startsWith('#')) {
+                a.setAttribute('target', '_blank');
+            }
+        });
 
         // Check if the authorinfo text has to be changed back to black/white
-        if ($(".authorinfo").length > 0 && $("header.image.singlepost").length > 0) {
-            verifyAuthorInfoColor();
-            $(window).scroll(verifyAuthorInfoColor);
+        if (authorInfoElm && headerImageElm) {
+            verifyAuthorInfoColor(headerImageElm, authorInfoElm);
+            window.addEventListener('scroll', verifyAuthorInfoColor.bind(this, headerImageElm, authorInfoElm));
         }
 
-        if ($(".postlist").length > 0) {
-            $("a.openmenu, .authorinfo").removeClass("white");
+        if (document.querySelector('.postlist') && authorInfoElm) {
+            authorInfoElm.classList.remove('white');
         }
-
-        if ($(".authorinfo").length) {
-            $(".showcontact").click(function (e) {
-                e.preventDefault();
-                $(".authorinfo, .contactoverlay").addClass("open white");
-                $("nav.mainmenu, a.openmenu").removeClass("open");
-                $("a.openmenu").addClass("closearrow");
-                $("header.main .profile").addClass("hidden");
-            });
-        }
-
-        $("a.openmenu").click(function (e) {
-            e.preventDefault();
-            if ($(this).hasClass("closearrow")) {
-                $(this).removeClass("closearrow");
-                $(".authorinfo, .contactoverlay").removeClass("open");
-                $("header.main .profile").removeClass("hidden");
-            }
-            else {
-                $("nav.mainmenu, a.openmenu").toggleClass("open");
-            }
-        });
-
-        $("nav.mainmenu .bt-overlay").click(function () {
-            $("nav.mainmenu, a.openmenu").removeClass("open");
-        });
-
-        $("a.share").click(function (e) {
-            e.preventDefault();
-            console.log();
-            var twitterurl = "https://twitter.com/intent/tweet?original_referer=" + $(this).attr('data-url') + "&text='" + $(".postdetails h1").first().text() + "'&tw_p=tweetbutton&url=" + $(this).attr('data-url') + "&via=dirkgroenen";
-            var googleurl = "https://plus.google.com/share?url=" + $(this).attr("data-url");
-
-            switch ($(this).attr("id")) {
-                case "twitter":
-                    window.open(twitterurl, 'Twitter', 'width=600,height=400,scrollbars=no,toolbar=no');
-                    break;
-                case "facebook":
-                    FB.ui({
-                        method: 'share',
-                        href: $(this).attr("data-url"),
-                    }, function (response) { });
-                    break;
-                case "googleplus":
-                    window.open(googleurl, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
-                    break;
-            }
-
-        });
     });
-
-})(jQuery);
+})();
